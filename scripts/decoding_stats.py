16b71a1d2d58a8b2fd5c2d65d1b471183612a50b
Benjamin Lipkin <lipkinb@node017.ib.cluster>
2021-03-01 17:44:22 -0500

    updated stats script

--- scripts/decoding_stats.py
+++ scripts/decoding_stats.py
@@ -1,22 +1,25 @@
 import pandas as pd
+from scipy.stats import ttest_1samp
 
-
-def get_data():
-    return pd.read_csv("../outputs/results.csv")
-
-
-def summarize(data):
+if __name__ == "__main__":
+    ostream = "NETWORK,FEATURE,N,NULL,MEAN,T,PVAL,H\n"
+    data = pd.read_csv("../outputs/results.csv")
     for network in data.NETWORK.unique():
         features = data[data.NETWORK == network]
         for feature in features.FEATURE.unique():
             subjects = features[features.FEATURE == feature]
-            print("%s      \t%s\t%.3f" % (network, feature, subjects.ACC.mean()))
-            # expand to run t-test and save ../outputs/stats.csv
-
-
-def main():
-    summarize(get_data())
-
-
-if __name__ == "__main__":
-    main()
+            null = subjects.P.values[0]
+            samples = subjects.ACC
+            tstat, pval = ttest_1samp(samples, null)
+            ostream += "%s,%s,%d,%f,%f,%f,%f,%d\n" % (
+                network,
+                feature,
+                samples.size,
+                null,
+                samples.mean(),
+                tstat,
+                pval,
+                int(pval < 0.05),
+            )
+    with open("../outputs/stats.csv", "w") as f:
+        f.write(ostream)
