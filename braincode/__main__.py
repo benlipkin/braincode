820f2c31e46e414c57b599d8e3e6ec1c468d2254
benlipkin <bglipkin@gmail.com>
2021-06-09 16:17:57 -0400

    refactored cli

--- braincode/__main__.py
+++ braincode/__main__.py
@@ -7,97 +7,92 @@ from joblib import Parallel, delayed
 from rsa import RSA
 
 
-def analysis_options():
-    return ["rsa", "mvpa", "prda"]
+class CLI:
+    def __init__(self):
+        self._default = "all"
+        self._analyses = [
+            "rsa",
+            "mvpa",
+            "prda",
+        ]
+        self._embeddings = [
+            "brain-lang",
+            "brain-MD",
+            "brain-aud",
+            "brain-vis",
+            "code-bow",
+            "code-tfidf",
+            # "code-seq2seq",
+        ]
+        self._features = [
+            "task-code",
+            "task-content",
+            "task-structure",
+            "code-bow",
+            "code-tfidf",
+            # "code-seq2seq",
+        ]
 
+    def _build_parser(self):
+        self._parser = ArgumentParser(description="run specified analysis type")
+        self._parser.add_argument("analysis", choices=self._analyses)
+        self._parser.add_argument(
+            "-e",
+            "--embedding",
+            choices=[self._default] + self._embeddings,
+            default=self._default,
+        )
+        self._parser.add_argument(
+            "-f",
+            "--feature",
+            choices=[self._default] + self._features,
+            default=self._default,
+        )
 
-def embedding_options():
-    return [
-        "brain-lang",
-        "brain-MD",
-        "brain-aud",
-        "brain-vis",
-        "code-bow",
-        "code-tfidf",
-        # "code-seq2seq",
-    ]
+    def _parse_args(self):
+        self._args = self._parser.parse_args()
 
+    def _clean_arg(self, arg, match, input):
+        arg = [opt for opt in arg if match in opt]
+        if len(arg) > 0:
+            return arg
+        else:
+            raise ValueError(
+                f"{self._args.analysis.upper()} only accepts '{match}' arguments for '{input}'."
+            )
 
-def feature_options():
-    return [
-        "task-code",
-        "task-content",
-        "task-structure",
-        "code-bow",
-        "code-tfidf",
-        # "code-seq2seq",
-    ]
+    def _prep_analysis(self):
+        if self._args.embedding != self._default:
+            self._embeddings = [self._args.embedding]
+        if self._args.feature != self._default:
+            self._features = [self._args.feature]
+        if self._args.analysis == "rsa":
+            if self._args.feature != self._default:
+                warnings.warn("rsa does not use feature; ignoring argument")
+            self._embeddings = self._clean_arg(self._embeddings, "brain", "-e")
+            self._params = [[embedding] for embedding in self._embeddings]
+        else:
+            if self._args.analysis == "mvpa":
+                self._embeddings = self._clean_arg(self._embeddings, "brain", "-e")
+            elif self._args.analysis == "prda":
+                self._embeddings = self._clean_arg(self._embeddings, "code", "-e")
+                self._features = self._clean_arg(self._features, "task", "-f")
+            else:
+                raise ValueError("Invalid argument for analysis.")
+            self._params = list(itertools.product(self._embeddings, self._features))
+        self._analysis = globals()[self._args.analysis.upper()]
 
+    def _run_analysis(self, param):
+        self._analysis(*param).run()
 
-def build_parser(default, analyses, embeddings, features):
-    parser = ArgumentParser(description="run specified analysis type")
-    parser.add_argument("analysis", choices=analyses)
-    parser.add_argument(
-        "-e", "--embedding", choices=[default] + embeddings, default=default
-    )
-    parser.add_argument(
-        "-f", "--feature", choices=[default] + features, default=default
-    )
-    return parser
-
-
-def clean_arg(arg, match, analysis, input):
-    arg = [opt for opt in arg if match in opt]
-    if len(arg) > 0:
-        return arg
-    else:
-        raise ValueError(
-            f"{analysis.upper()} only accepts '{match}' arguments for '{input}'."
+    def run_main(self):
+        self._build_parser()
+        self._parse_args()
+        self._prep_analysis()
+        Parallel(n_jobs=len(self._params))(
+            delayed(self._run_analysis)(param) for param in self._params
         )
 
 
-def rsa_analysis(embedding):
-    RSA(embedding).run()
-
-
-def mvpa_analysis(embedding, feature):
-    MVPA(embedding, feature).run()
-
-
-def prda_analysis(embedding, feature):
-    PRDA(embedding, feature).run()
-
-
-def prep_analysis(args, default, analyses, embeddings, features):
-    if args.embedding != default:
-        embeddings = [args.embedding]
-    if args.feature != default:
-        features = [args.feature]
-    if args.analysis == analyses[0]:
-        if args.feature != default:
-            warnings.warn("rsa does not use feature; ignoring argument")
-        embeddings = clean_arg(embeddings, "brain", args.analysis, "-e")
-        params = [[embedding] for embedding in embeddings]
-        function = rsa_analysis
-    elif args.analysis == analyses[1]:
-        embeddings = clean_arg(embeddings, "brain", args.analysis, "-e")
-        params = list(itertools.product(embeddings, features))
-        function = mvpa_analysis
-    elif args.analysis == analyses[2]:
-        embeddings = clean_arg(embeddings, "code", args.analysis, "-e")
-        features = clean_arg(features, "task", args.analysis, "-f")
-        params = list(itertools.product(embeddings, features))
-        function = prda_analysis
-    else:
-        raise ValueError("Invalid Argument")
-    return params, function
-
-
 if __name__ == "__main__":
-    default = "all"
-    analyses = analysis_options()
-    embeddings = embedding_options()
-    features = feature_options()
-    args = build_parser(default, analyses, embeddings, features).parse_args()
-    params, function = prep_analysis(args, default, analyses, embeddings, features)
-    Parallel(n_jobs=len(params))(delayed(function)(*param) for param in params)
+    CLI().run_main()
