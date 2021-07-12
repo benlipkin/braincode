3ef9266058b662182fed0d38363ebc2342c5c0f3
benlipkin <bglipkin@gmail.com>
2021-07-12 17:38:03 -0400

    added timer

--- braincode/__main__.py
+++ braincode/__main__.py
@@ -1,7 +1,11 @@
 import logging
 import sys
+from datetime import datetime
 from parser import CLI
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    start = datetime.now()
     CLI().run_main()
+    elapsed = datetime.now() - start
+    logging.getLogger(__name__).info(f"Completed successfully in {elapsed}.")
