cb46f577f5170960baa65e383d2f4bd82ec7c159
benlipkin <bglipkin@gmail.com>
2021-07-28 13:05:23 -0400

    progress on code-transformer setup

--- setup/_setup_code_transformer.sh
+++ setup/_setup_code_transformer.sh
@@ -6,15 +6,19 @@ conda install -yc anaconda wget
 conda install -yc conda-forge tar
 conda install -yc ostrokach gzip
 
-NAME=code_transformer/
-CACHE_DIR=$(pwd)/../braincode/outputs/cache/
+NAME=code_transformer
+CACHE_DIR=$(dirname $(pwd))/braincode/outputs/cache
 DATASET_DIR=$CACHE_DIR/datasets/$NAME
 MODEL_DIR=$CACHE_DIR/models/$NAME
 BINARY_DIR=$CACHE_DIR/bin/$NAME
+LOG_DIR=$CACHE_DIR/logs/$NAME
+ENV_DIR=$HOME/.config/$NAME
 
 mkdir -p $DATASET_DIR
 mkdir -p $MODEL_DIR
 mkdir -p $BINARY_DIR
+mkdir -p $LOG_DIR
+mkdir -p $ENV_DIR
 
 cd $DATASET_DIR
 wget -O python.tar.gz https://syncandshare.lrz.de/dl/fi5NDSSUYPnEQ2D6zga4XtN5/python.tar.gz
@@ -36,5 +40,22 @@ cd $BINARY_DIR
 wget -O semantic.tar.gz https://syncandshare.lrz.de/dl/fiK3DkYhvPaS1sENaGuABvi8/semantic.tar.gz
 tar -xvzf semantic.tar.gz
 
-cd $CACHE_DIR
-gunzip -r .
+cd $ENV_DIR
+echo "
+export CODE_TRANSFORMER_DATA_PATH=$DATASET_DIR
+export CODE_TRANSFORMER_BINARY_PATH=$BINARY_DIR
+export CODE_TRANSFORMER_MODELS_PATH=$MODEL_DIR
+export CODE_TRANSFORMER_LOGS_PATH=$LOG_DIR
+
+export CODE_TRANSFORMER_CSN_RAW_DATA_PATH=$DATASET_DIR/raw/csn
+export CODE_TRANSFORMER_CODE2SEQ_RAW_DATA_PATH=$DATASET_DIR/raw/code2seq
+export CODE_TRANSFORMER_CODE2SEQ_EXTRACTED_METHODS_DATA_PATH=$DATASET_DIR/raw/code2seq-methods
+
+export CODE_TRANSFORMER_DATA_PATH_STAGE_1=$DATASET_DIR/stage1
+export CODE_TRANSFORMER_DATA_PATH_STAGE_2=$DATASET_DIR/stage2
+
+export CODE_TRANSFORMER_JAVA_EXECUTABLE=java
+export CODE_TRANSFORMER_JAVA_PARSER_EXECUTABLE=$BINARY_DIR/java-parser-1.0-SNAPSHOT.jar
+export CODE_TRANSFORMER_JAVA_METHOD_EXTRACTOR_EXECUTABLE=$BINARY_DIR/JavaMethodExtractor-1.0.0-SNAPSHOT.jar
+export CODE_TRANSFORMER_SEMANTIC_EXECUTABLE=$BINARY_DIR/semantic
+" > $ENV_DIR/.env
