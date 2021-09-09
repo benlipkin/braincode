5dfc3d4244aa5710e2d3a1e36c894ed5583400cd
benlipkin <38060297+benlipkin@users.noreply.github.com>
2021-09-09 17:38:20 -0400

    update setup.sh default to download pre-trained model

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,5 +3,5 @@
 # $1 can be  "$(dirname $(pwd))"
 ##################################
 bash _setup_inputs.sh $1 
-bash _setup_code_seq2seq.sh $1 False True True True True False
+bash _setup_code_seq2seq.sh $1 True False False False False False
 bash _setup_code_transformer.sh $1
