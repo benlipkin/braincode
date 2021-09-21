9cbaaf626d8b3ccd0ba6fcc030aef7225174edd6
shash <shash@mit.edu>
2021-09-21 19:39:20 +0000

    tweaks to utils clearing fns

--- braincode/utils.py
+++ braincode/utils.py
@@ -6,18 +6,38 @@ import sys
 import numpy as np
 
 
-def clean_cache(pth):
-    folders = os.listdir(pth)
-    print("Clear path? {}".format(pth))
-    inp = input()
-    if "y" in inp.lower() or "1" in inp.lower():
-        for ff in folders:
-            files = os.listdir(os.path.join(pth, ff))
-            for f in files:
-                if "score" in f or ".npy" in f:
-                    print("Clearing {}".format(os.path.join(pth, ff, f)))
-                    os.remove(os.path.join(pth, ff, f))
-
+def clean_cache(base_pth, choice):
+    def _clean_cache(choice):
+        if choice == 2:
+            folder_name = "scores"
+        elif choice == 3:
+            folder_name = "representations"
+        elif choice == 4:
+            folder_name = "profiler"
+        
+        pth = os.path.join(base_pth, folder_name)
+        print("Clear path? {}".format(pth))
+        inp = input()
+        if "y" in inp.lower() or "1" in inp.lower():
+            for dir, _, filenames in os.walk(pth):
+                for f in filenames:
+                    if folder_name == "scores":
+                        condition = "score" in f or ".npy" in f
+                    elif folder_name == "representations":
+                        condition = "pkl" in f
+                    elif folder_name == "profiler":
+                        condition = ".lprof" in f or ".py" in f
+                    
+                    if condition:
+                        print("Clearing {}".format(os.path.join(pth, dir, f)))
+                        os.remove(os.path.join(pth, dir, f))
+    # clear scores
+    if choice == 0:
+        for i in [2, 3, 4]:
+            _clean_cache(i)
+    else:
+        _clean_cache(choice)
+    
 
 def print_scores(pth):
     scores = {}
@@ -43,6 +63,6 @@ if __name__ == "__main__":
     choice = int(sys.argv[2])
 
     if choice == 1:
-        clean_cache(pth)
-    elif choice == 2:
         print_scores(pth)
+    elif choice in [0, 2, 3, 4]:
+        clean_cache(pth, choice)
