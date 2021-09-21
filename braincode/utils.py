43a7bb924d33df2c10f8698a1920b1dfc2ecb2aa
shash <shash@mit.edu>
2021-09-21 15:27:25 +0000

    modified utils according to most recent folder structure in scores dump

--- braincode/utils.py
+++ braincode/utils.py
@@ -7,29 +7,33 @@ import numpy as np
 
 
 def clean_cache(pth):
-    files = os.listdir(pth)
+    folders = os.listdir(pth)
     print("Clear path? {}".format(pth))
     inp = input()
     if "y" in inp.lower() or "1" in inp.lower():
-        for f in files:
-            if "score" in f and ".npy" in f:
-                print("Clearing {}".format(os.path.join(pth, f)))
-                os.remove(os.path.join(pth, f))
+        for ff in folders:
+            files = os.listdir(os.path.join(pth, ff))
+            for f in files:
+                if "score" in f and ".npy" in f:
+                    print("Clearing {}".format(os.path.join(pth, ff, f)))
+                    os.remove(os.path.join(pth, ff, f))
 
 
 def print_scores(pth):
     scores = {}
-    files = os.listdir(pth)
-    for f in files:
-        if "score" in f and ".npy" in f:
-            names = "".join(f.split(".npy")[:-1]).split("_")
-            if len(names) == 3:
-                feature = names[1]
-                target = names[2]
-                scores_np = np.load(os.path.join(pth, f))
-                if feature not in scores:
-                    scores[feature] = {}
-                scores[feature][target] = scores_np.item()
+    folders = os.listdir(pth)
+    for ff in folders:
+        files = os.listdir(os.path.join(pth, ff))
+        for f in files:
+            if "score" in f and ".npy" in f:
+                names = "".join(f.split(".npy")[:-1]).split("_")
+                if len(names) == 3:
+                    feature = names[1]
+                    target = names[2]
+                    scores_np = np.load(os.path.join(pth, ff, f))
+                    if feature not in scores:
+                        scores[feature] = {}
+                    scores[feature][target] = scores_np.item()
 
     print("Model-wise scores: \n{}".format(json.dumps(scores, indent=2)))
 
