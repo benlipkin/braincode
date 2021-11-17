3223647795334a1f7e24831916006d3c95a0b850
benlipkin <bglipkin@gmail.com>
2021-11-17 18:37:10 -0500

    run paper analyses

--- paper/scripts/run.sh
+++ paper/scripts/run.sh
@@ -3,8 +3,3 @@ python scores.py
 python stats.py
 python plots.py
 python tables.py
-cd supplemental/property_corrs
-python code_property_corrs.py
-cd ../model_dims
-python model_dim_expt.py
-cd ../../
