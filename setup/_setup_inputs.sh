f31e45f163dc761f92f16b3c662d5556706f34fa
benlipkin <38060297+benlipkin@users.noreply.github.com>
2021-11-20 17:36:56 -0500

    updated inputs link

--- setup/_setup_inputs.sh
+++ setup/_setup_inputs.sh
@@ -8,7 +8,7 @@ conda install -yc conda-forge unzip
 HOME_DIR=$1/braincode/
 cd $HOME_DIR
 
-wget -O inputs.zip https://www.dropbox.com/s/78e0r8kmmnwfa1m/inputs.zip?dl=0
+wget -O inputs.zip https://www.dropbox.com/s/63b16bx3hrx0rar/inputs.zip?dl=0
 unzip inputs.zip -d inputs
 
 # Process and populate benchmark metrics on input files
