8956e326e9d1f0bcc4c354143bf13776aea12c51
benlipkin <bglipkin@gmail.com>
2022-03-03 13:35:56 -0500

    moved rsm calculation to metrics and exposed internal args

--- braincode/metrics.py
+++ braincode/metrics.py
@@ -10,23 +10,22 @@ class Metric(ABC):
     def __init__(self):
         pass
 
-    def __call__(self, Y_pred, Y_true):
-        if Y_pred.ndim == 1:
-            Y_pred = Y_pred.reshape(-1, 1)
-        if Y_true.ndim == 1:
-            Y_true = Y_true.reshape(-1, 1)
-        if any(y.ndim != 2 for y in [Y_pred, Y_true]):
-            raise ValueError("Y_pred and Y_true must be 1D or 2D arrays.")
-        if not Y_pred.shape[0] == Y_true.shape[0]:
-            raise ValueError("Y_pred and Y_true must have the same number of samples.")
-        if not Y_pred.shape[1] == Y_true.shape[1]:
-            raise ValueError(
-                "Y_pred and Y_true must have the same number of dimensions."
-            )
-        return self._apply_metric(Y_pred, Y_true)
+    def __call__(self, X, Y):
+        if X.ndim == 1:
+            X = X.reshape(-1, 1)
+        if Y.ndim == 1:
+            Y = Y.reshape(-1, 1)
+        if any(y.ndim != 2 for y in [X, Y]):
+            raise ValueError("X and Y must be 1D or 2D arrays.")
+        if X.shape[0] != Y.shape[0]:
+            raise ValueError("X and Y must have the same number of samples.")
+        if self.__class__.__name__ != "RepresentationalSimilarity":
+            if X.shape[1] != Y.shape[1]:
+                raise ValueError("X and Y must have the same number of dimensions.")
+        return self._apply_metric(X, Y)
 
     @abstractmethod
-    def _apply_metric(self, Y_pred, Y_true):
+    def _apply_metric(self, X, Y):
         raise NotImplementedError("Handled by subclass.")
 
 
@@ -35,16 +34,16 @@ class VectorMetric(Metric):
         self._reduce = reduce
         super().__init__()
 
-    def _apply_metric(self, Y_pred, Y_true):
-        scores = np.zeros(Y_pred.shape[1])
+    def _apply_metric(self, X, Y):
+        scores = np.zeros(X.shape[1])
         for i in range(scores.size):
-            scores[i] = self._score(Y_pred[:, i], Y_true[:, i])
+            scores[i] = self._score(X[:, i], Y[:, i])
         if self._reduce:
             return scores.mean()
         return scores
 
     @abstractmethod
-    def _score(self, y_pred, y_true):
+    def _score(self, X, Y):
         raise NotImplementedError("Handled by subclass.")
 
 
@@ -52,47 +51,47 @@ class MatrixMetric(Metric):
     def __init__(self):
         super().__init__()
 
-    def _apply_metric(self, Y_pred, Y_true):
-        score = self._score(Y_pred, Y_true)
+    def _apply_metric(self, X, Y):
+        score = self._score(X, Y)
         return score
 
     @abstractmethod
-    def _score(self, y_pred, y_true):
+    def _score(self, X, Y):
         raise NotImplementedError("Handled by subclass.")
 
 
 class PearsonR(VectorMetric):
     @staticmethod
-    def _score(y_pred, y_true):
-        r, p = pearsonr(y_pred, y_true)
+    def _score(x, y):
+        r, p = pearsonr(x, y)
         return r
 
 
 class SpearmanRho(VectorMetric):
     @staticmethod
-    def _score(y_pred, y_true):
-        rho, p = spearmanr(y_pred, y_true)
+    def _score(x, y):
+        rho, p = spearmanr(x, y)
         return rho
 
 
 class KendallTau(VectorMetric):
     @staticmethod
-    def _score(y_pred, y_true):
-        tau, p = kendalltau(y_pred, y_true)
+    def _score(x, y):
+        tau, p = kendalltau(x, y)
         return tau
 
 
 class RMSE(VectorMetric):
     @staticmethod
-    def _score(y_pred, y_true):
-        loss = mean_squared_error(y_pred, y_true, squared=False)
+    def _score(x, y):
+        loss = mean_squared_error(x, y, squared=False)
         return loss
 
 
 class ClassificationAccuracy(VectorMetric):
     @staticmethod
-    def _score(y_pred, y_true):
-        score = accuracy_score(y_true, y_pred, normalize=True)
+    def _score(x, y):
+        score = accuracy_score(x, y, normalize=True)
         return score
 
 
@@ -101,9 +100,22 @@ class RankAccuracy(MatrixMetric):
         self._distance = distance
         super().__init__()
 
-    def _score(self, Y_pred, Y_true):
-        distances = pairwise_distances(Y_pred, Y_true, metric=self._distance)
+    def _score(self, X, Y):
+        distances = pairwise_distances(X, Y, metric=self._distance)
         scores = (distances.T > np.diag(distances)).sum(axis=0) / (
             distances.shape[1] - 1
         )
         return scores.mean()
+
+
+class RepresentationalSimilarity(MatrixMetric):
+    def __init__(self, distance="correlation", comparison=PearsonR()):
+        self._distance = distance
+        self._comparison = comparison
+
+    def _score(self, X, Y):
+        X_rdm = pairwise_distances(X, metric=self._distance)
+        Y_rdm = pairwise_distances(Y, metric=self._distance)
+        indices = np.triu_indices(X_rdm.shape[0], k=1)
+        score = self._comparison(X_rdm[indices], Y_rdm[indices])
+        return score
--- braincode/rsa.py
+++ braincode/rsa.py
@@ -1,19 +1,11 @@
 import numpy as np
 from analyses import BrainAnalysis
-from scipy.stats import pearsonr
+from metrics import RepresentationalSimilarity
 
 
 class RSA(BrainAnalysis):
     def __init__(self, feature, target, kwargs):
         super().__init__(feature, target, **kwargs)
-
-    @staticmethod
-    def _calc_rsa(brain_rdm, model_rdm):
-        if not brain_rdm.matrix.size == model_rdm.matrix.size:
-            raise RuntimeError("RDMs mismatched. Check feature target pair.")
-        indices = np.triu_indices(brain_rdm.matrix.shape[0], k=1)
-        score, pval = pearsonr(brain_rdm.matrix[indices], model_rdm.matrix[indices])
-        return score
 
     def _load_subject(self, subject):
         X, Y, _ = self._loader.get_data(self.__class__.__name__.lower(), subject)
@@ -24,23 +16,6 @@ class RSA(BrainAnalysis):
         return Y
 
     def _score(self, X, Y, _):
-        score = self._calc_rsa(RDM(self._feature, X), RDM(self._target, Y))
+        metric = RepresentationalSimilarity("correlation")
+        score = metric(X, Y)
         return score
-
-
-class RDM:
-    def __init__(self, name, samples):
-        self._name = name
-        self._samples = samples
-        self._matrix = self._calc_matrix()
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def matrix(self):
-        return self._matrix
-
-    def _calc_matrix(self):
-        return 1 - np.corrcoef(self._samples)
