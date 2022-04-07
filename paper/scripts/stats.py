cb11d5500f8916c193c7e7a7ee14a2431ac6bbbe
benlipkin <bglipkin@gmail.com>
2022-04-07 18:11:34 -0400

    finalized stats script

--- paper/scripts/stats.py
+++ paper/scripts/stats.py
@@ -27,7 +27,7 @@ def calc_stats(dataset, reverse_grouping=False):
                     continue
             s1 = samples[samples["Feature"] == p1].iloc[0, 2:].values
             s2 = samples[samples["Feature"] == p2].iloc[0, 2:].values
-            t, p = st.ttest_rel(s1, s2, alternative="two-sided")
+            t, p = st.ttest_rel(s1, s2)
             groupings.append(grouping)
             S1.append(p1)
             S2.append(p2)
